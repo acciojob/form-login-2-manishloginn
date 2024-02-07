65234097863a4f80ad2de85eba6a09f029030c9f
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-07 22:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+const submitButton = document.getElementById("submitButton");
+const data = {};
+ 
+function showAlert() {
+  event.preventDefault();
+  data.firstName = document.getElementById("firstName").value;
+  data.lastName = document.getElementById("lastName").value;
+  data.phoneNumber = document.getElementById("phoneNumber").value;
+  data.emailID = document.getElementById("emailID").value;
+ 
+  alert(
+    "First Name: " +
+      data.firstName +
+      "\nLast Name: " +
+      data.lastName +
+      "\nPhone Number: " +
+      data.phoneNumber +
+      "\nEmail ID: " +
+      data.emailID
+  );
+}
+ 
+submitButton.addEventListener("click", showAlert);
